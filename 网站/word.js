d964f1ddf40e0b7c7dc0a54d0a2f08d9a410bd5a
zimowuer <z15060071585@163.com>
2023-08-21 19:41:09 +0800

    more better

---
+++ 网站/word.js
@@ -0,0 +1,81 @@
+// word.js
+const contentData = {
+    games: [
+        {
+            title: "Minecraft 2D(电脑)",
+            description: "操作方式在游戏开始界面。",
+            link: "网站/我的世界.html",
+            imgSrc: "素材/我的世界.jpeg"
+        },
+        {
+            title: "我的世界3D(电脑)",
+            description: "操作方式，wsad移动。",
+            link: "网站/3D版我的世界.html",
+            imgSrc: "素材/我的世界.jpeg"
+        },
+        {
+            title: "元气骑士(手机/电脑)",
+            description: "操作方式，wsad移动+鼠标点击，手机端屏幕按键。",
+            link: "网站/元气骑士破解版.html",
+            imgSrc: "素材/三国杀ISO.ico"
+        },
+        {
+            title: "飞(手机/电脑)",
+            description: "操作方式，鼠标点击。",
+            link: "网站/飞.html",
+            imgSrc: "素材/内鬼.jpg"
+        },
+        {
+            title: "坑爹跑酷 (电脑)(有挂，按n键)",
+            description: "2D游戏,操作方式，wad控制方向,有外挂，按n键传送至鼠标当前位置",
+            link: "网站/坑爹跑酷 (有挂，按n键).html",
+            imgSrc: "素材/火.ico"
+        },
+        {
+            title: "古代战争模拟器(手机/电脑)",
+            description: "鼠标点击",
+            link: "网站/古代战争模拟器.html",
+            imgSrc: "素材/拳王.jpeg"
+        },
+        {
+            title: "古代战争模拟器(手机/电脑)",
+            description: "鼠标点击",
+            link: "网站/古代战争模拟器.html",
+            imgSrc: "素材/拳王.jpeg"
+        }
+        // 可以添加更多游戏内容对象
+    ],
+    tools: [
+        {
+            title: "工具1",
+            description: "这是工具1的介绍。"
+        },
+        {
+            title: "工具2",
+            description: "这是工具2的介绍。"
+        }
+        // 可以添加更多工具内容对象
+    ],
+    media: [
+        {
+            title: "视频1",
+            description: "这是视频1的介绍。"
+        },
+        {
+            title: "音乐1",
+            description: "这是音乐1的介绍。"
+        }
+        // 可以添加更多媒体内容对象
+    ],
+    thanks: [
+        {
+            text: "thanks for open ai"
+        },
+        {
+            text: "thanks for github"
+        }
+        // 可以添加更多鸣谢内容对象
+    ]
+};
+
+export default contentData;
